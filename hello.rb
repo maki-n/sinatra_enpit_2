7cceff38ec2129bfcf051018a54e43c9aab0b12b
maki-n <z16081nm@aiit.ac.jp>
2017-07-14 10:07:16 +0000

    Add devided rb files

--- hello.rb
+++ hello.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require './maki-n'
+require './a1702ti'
 
 template :layout do
   "<html><body><h1>Hello World</h1><%= yield %></body></html>"
@@ -30,4 +32,8 @@ end
 
 get '/test2' do
   erb :test2
+end
+
+git '/test3' do
+  erb :test3, locals => { md => markdown(:test3) }
 end
