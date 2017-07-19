6cb1e829c451c4a77bf49ac8625695918906f696
maki-n <z16081nm@aiit.ac.jp>
2017-07-19 10:00:37 +0000

    test

--- hello.rb
+++ hello.rb
@@ -1,12 +1,3 @@
 require 'sinatra'
-<<<<<<< HEAD
 require './maki-n.rb'
-=======
-require './maki-n' 
-require './a1702ti'
-<<<<<<< HEAD
->>>>>>> bdc40e88f37674f3763de4f4256bb1ed2d627249
-
-
-=======
->>>>>>> fbcfcab69cd85b44bdc758d8f4979874e67582db
+#require './a1702ti'
