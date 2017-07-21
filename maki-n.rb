899fa1c5da06fb45a4a026af0a5f99f5c676bb28
maki-n <z16081nm@aiit.ac.jp>
2017-07-21 11:10:51 +0900

    Update maki-n.rb

--- maki-n.rb
+++ maki-n.rb
@@ -1,22 +1,20 @@
 require 'sinatra'
 
-template:layout do 
-"<h3>サブタイトル１</h3><%= yield %>"
-end 
-
 get '/test1' do
 erb %{
+<h2>サブタイトル１</h2>
 <p><a href="./greeting1">あいさつ</a></p>
 <p><a href="./test1-2">test1-2</a></p>
 <p><a href="./test1-3">test1-3</a></p>
-<p><a href="./hello">戻る</a></p>
+<p><a href="./">戻る</a></p>
 }
 end
   
 get '/greeting1' do
 erb %{
+<h2>サブタイトル１</h2><h3>あいさつ</h3>
 <form action="./greeting2" method="post">
-<div>名前を入力してください<input type="text" name="name" value="太郎"></div>
+<div>名前を入力してください<input type="text" name="name" value="Taro"></div>
 <div><input type="submit" value="決定"></div>
 </form>
 }
@@ -25,6 +23,7 @@ end
 post '/greeting2' do
 @name = params[:name]
 erb %{
+<h2>サブタイトル１</h2><h3>あいさつ</h3>
 <p>Hello <%= @name %>.<br>How are you?</p>
 <p><a href="./test1">戻る</a></p>
 }
@@ -34,6 +33,7 @@ get '/test1-2' do
 @a = 1
 @b = 2
 erb %{
+<h2>サブタイトル１</h2><h3></h3>
 <%= @a %>+<%= @b %>=<%= @a+@b %><br>
 }
 end
