4eba554035e81382b261b3e58cafd8da1cfde73b
maki-n <z16081nm@aiit.ac.jp>
2017-07-21 09:48:45 +0900

    Update maki-n.rb

--- maki-n.rb
+++ maki-n.rb
@@ -1,22 +1,31 @@
-# -*- coding: utf-8 -*-
 require 'sinatra'
 
 get '/test1' do
-  erb %{
-<p><a href="./test1-1">test1-1</a></p>
+erb %{
+<p><a href="./greeting1">あいさつ</a></p>
 <p><a href="./test1-2">test1-2</a></p>
 <p><a href="./test1-3">test1-3</a></p>
 }
 end
-
-get '/test1-2/*' do |name|
-  erb %{
-hello #{name}.<br>
-how are you?
+  
+get '/greeting1' do
+erb %{
+<form action=".greeting2" method="post">
+<div>名前<input type="text" name="name" value="名前を入力してください"></div>
+<div><input type="submit" value="決定"></div>
+</form>
 }
 end
-  
-get '/test1-1' do
+
+get '/greeting2' do
+@name = params[:name]
+erb %{
+<p>Hello #{name}.<br>How are you?</p>
+<p><a href="./test1">戻る</a><p>
+}
+end
+
+get '/test1-2' do
   @a = 1
   erb %{
 1+1= <%= 1+1 %><br>
